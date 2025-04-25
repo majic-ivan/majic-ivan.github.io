14bd80f144654ef0450d3ec28299303bfe8b11b5
Ivan Majic <ivanmajic31@gmail.com>
2025-04-25 17:38:14 +0100

    Added fade in and fixed footer

--- script.js
+++ script.js
@@ -4,3 +4,17 @@ const navList = document.querySelector('.nav-list');
 mobileMenu.addEventListener('click', () => {
     navList.classList.toggle('active');
 });
+
+document.addEventListener('DOMContentLoaded', () => {
+    const fadeInElements = document.querySelectorAll('.fade-in');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('visible');
+            }
+        });
+    });
+
+    fadeInElements.forEach((el) => observer.observe(el));
+});
